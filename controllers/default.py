6a31d488a6b0991eedb27f94f7bb9a9507a6fe0f
Mariano Reingart <reingart@gmail.com>
2011-12-18 04:42:13 -0300

    PyConAr2012: ajustes iniciales menores (estilo y + share)

---
+++ controllers/default.py
@@ -0,0 +1,68 @@
+#############################################
+### general variables
+#############################################
+
+from gluon.sqlhtml import form_factory
+
+#############################################
+# The main public page
+#############################################
+
+#@cache(request.env.path_info,time_expire=60*5,cache_model=cache.ram)
+def index():
+    response.files.append(URL(r=request,c='static',f='jquery-slideshow.css'))
+    response.files.append(URL(r=request,c='static',f='jquery-slideshow.js'))
+    session.forget()
+    # do not cache flatpage edits!:
+    if request.vars or request.args or request.flash or session.flash or auth.is_logged_in():
+        r = response.render(plugin_flatpage()) 
+    else:
+        r = cache.ram(request.env.path_info,lambda: response.render(plugin_flatpage()), time_expire=60*5)
+    return r
+
+#@cache(request.env.path_info,time_expire=60*15,cache_model=cache.ram)
+def about():
+    return response.render(dict())
+
+@cache(request.env.path_info,time_expire=60*15,cache_model=cache.ram)
+def twitter():
+    session.forget()
+    session._unlock(response)
+    import gluon.tools
+    import gluon.contrib.simplejson as sj
+    try:
+        if TWITTER_HASH:
+            page = gluon.tools.fetch('http://twitter.com/%s?format=json'%TWITTER_HASH)
+            return sj.loads(page)['#timeline']
+        else:
+            return 'disabled'
+    except Exception, e:
+        return DIV(T('Unable to download because:'),BR(),str(e))
+
+#############################################
+# Allow registered visitors to download
+#############################################
+
+@auth.requires_login()
+def download(): 
+    return response.download(request,db)
+
+###@cache(request.env.path_info,60,cache.disk)
+def fast_download():
+    # very basic security:
+    if not request.args(0).startswith("sponsor.logo") and not request.args(0).startswith("auth_user.photo") and not request.args(0).startswith("attachment.file"): 
+        return download()
+    if 'filename' in request.vars:
+        response.headers["Content-Disposition"] = "attachment; filename=%s" % request.vars['filename'] 
+
+    # remove/add headers that prevent/favors caching
+    del response.headers['Cache-Control']
+    del response.headers['Pragma']
+    del response.headers['Expires']
+    filename = os.path.join(request.folder,'uploads',request.args(0))
+    response.headers['Last-Modified'] = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(os.path.getmtime(filename)))
+    return response.stream(open(filename,'rb'))
+
+def notify():
+    response.headers['Content-Type']='text/xml'
+    return l2controller.receive_xml(request.body.read())
---
+++ controllers/user.py
@@ -0,0 +1,58 @@
+#############################################
+# Manage Authentication
+#############################################
+
+crud=Crud(globals(),db)
+
+# we are not in default controller, change it at auth
+auth.settings.controller='user'
+
+def login():
+    return dict(form=auth.login(#next=URL(r=request,c='user',f='profile'),
+                                onaccept=lambda form:update_pay(auth.user)))
+
+def verify():
+    return auth.verify_email(next=URL(r=request,f='login'))
+
+def register():
+    form=auth.register(next=URL(r=request,c='default',f='index'),
+                       onaccept=update_person)
+    return dict(form=form)
+                
+def password():
+    return dict(form=auth.retrieve_password(next='login'))
+        
+
+@auth.requires_login()
+def logout(): auth.logout(next=URL(r=request,c='default',f='index', args="nocache"))
+
+@auth.requires_login()
+def profile():
+    you=db.auth_user.id==auth.user.id
+    person=db(you).select()[0]
+    require_address(person)
+    if person.amount_paid>0 or person.amount_subtracted>0:
+        db.auth_user.donation_to_PSF.writable=False
+        db.auth_user.attendee_type.writable=False
+        db.auth_user.discount_coupon.writable=False
+    form=crud.update(db.auth_user,auth.user.id,
+                     onaccept=update_person,
+                     next='profile')
+    return dict(form=form)
+
+
+def confirm():
+    if len(request.args)==2:
+        user_id = request.args[0]
+        received_hash = request.args[1]
+        q = db.auth_user.id==user_id
+        u = db(q).select()[0]
+        s= "%s-%s-%s-%s-%s" % (u.last_name, u.first_name, u.email, u.created_by_ip, u.created_on)
+        import hashlib
+        computed_hash = hashlib.md5(s).hexdigest()
+        if computed_hash == received_hash:
+            db(q).update(confirmed=True)
+            session.flash = T("Confirmed!")
+            redirect(URL(c="default", f="index", args="nocache"))
+    session.flash = T("Not Confirmed, please enter into your profile and check '%s' field") % T("Confirm attendance")
+    redirect(URL(c="user", f="profile"))
